4ebfe1433f3e87151386a94c529d590df64c60df
Zubair <57180013+MZubairLohar@users.noreply.github.com>
2021-04-18 17:38:29 +0500

    completed

---
+++ client/src/components/Loader.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Loader.css'
+const Loader = () => {
+    return (
+        <div className= 'loading'>
+            <img className='loading_image' src='images/loader.gif' alt='loader'/>
+        </div>
+    )
+}
+
+export default Loader
